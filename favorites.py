4b3c6560f08a913290f1996cc0d6218f6ac7464b
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-27 13:52:43 -0400

    Updated from Brython Server: 9/27/2017 1:52:43 PM

--- favorites.py
+++ favorites.py
@@ -1,1 +1,9 @@
+#Morgan Baughman
+#9/27/17
+#favorites.py
 
+word = input('Enter your favorite word: ')
+number = int(input('Enter your favorite number: '))
+
+for i in range(1,number+1):
+    print(word)
