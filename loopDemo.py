b8f6c87caa7b8d42cfb68bbbe5940891015061dd
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-27 13:45:34 -0400

    Updated from Brython Server: 9/27/2017 1:45:34 PM

--- loopDemo.py
+++ loopDemo.py
@@ -7,7 +7,37 @@
 for i in range(0,5):
     print('I love computer science')
 """
-
+"""
+i = 1
+while i <= 5:
+    print('I love computer science')
+    i = i+1
+"""
+"""
 #print the numbers from 1 to 20
 for i in range(1,21):
+    print(i**3)
+"""
+"""
+i = 1
+while i <=20:
     print(i)
+    i = i +1"""
+"""
+#print out the off numbers from 13 to 27
+for i in range(13,28,2): #(starting number, ending number-1, number it goes up by)
+    print(i)
+"""
+"""
+#add up the numbers 1 to 5
+total = 0
+for i in range(1,6):
+    total = total + i 
+print(total)"""
+
+total = 0
+i = 0
+while i <=5:
+    total = total + i 
+    i = i+1 #i+=1 is the same as i + i+1
+print(total)
