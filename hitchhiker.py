443335b8d8dae640db84ec2b842c56654b52a15c
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-28 14:36:35 -0400

    Updated from Brython Server: 9/28/2017 2:36:35 PM

--- hitchhiker.py
+++ hitchhiker.py
@@ -1,1 +1,11 @@
+#Morgan Baughman
+#9/28/17
+#hitchhiker.py
 
+i = 1
+while i <= 5:
+    question = input('Ask a question or type "quit": ')
+    if question == 'quit':
+        break
+    else:
+        print('42')
