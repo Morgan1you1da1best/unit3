53eb7f9f406bcf5a083a0fc2d36dbf2d2f40a589
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-09-28 14:54:41 -0400

    Updated from Brython Server: 9/28/2017 2:54:40 PM

--- unitConverter.py
+++ unitConverter.py
@@ -1,1 +1,21 @@
+#Morgan Baughman
+#9/28/17
+#unitConverter.py - COnverting Units of stuff
 
+print('choose a convertion.')
+print('a) Kilotmeters to Miles')
+print('b) Kilograms to Pounds')
+print('c) Liters to Gallons.')
+print('d) Celsius to Fahrenheit')
+convertionopt = (input('choose a convertion: '))
+value = float(input('Enter value: '))
+
+a = value/0.621371
+b = value/2.20462
+c = value/0.264172
+d = value/32
+
+print('Kilotmeters to Miles' , a)
+print('Kilograms to Pounds' , b)
+print(value, 'Liters =', c ,'Gallons')
+print( value, 'Celsius =', d, 'Fahrenheit' )
