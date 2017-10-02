face37544a03b3a0096eaaf4b81fd2e2a77d68ef
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-10-02 14:53:50 -0400

    Updated from Brython Server: 10/2/2017 2:53:50 PM

--- fri13.py
+++ fri13.py
@@ -1,1 +1,11 @@
+#Morgan Baughman
+#10/2/17
+#fri13.py - find the first 10 friday the thirteenth. 
 
+#weekday(years,days,months)
+
+import weekday
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
