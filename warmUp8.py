a5b39f5a6322ab1e783a3d53c342df77db79a3f4
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-10-02 14:35:56 -0400

    Updated from Brython Server: 10/2/2017 2:35:49 PM

--- warmUp8.py
+++ warmUp8.py
@@ -1,1 +1,11 @@
-
+#Morgan Baughman 
+#10/2/17 
+#warpUp8.py 
+total = 0 
+i = 1 
+while i <=100000: 
+    if i%3 == 0 and i%10 == 0 and i %17 == 0: 
+print(i) 
+i = i + 1 
+total = total + i 
+print(total)
