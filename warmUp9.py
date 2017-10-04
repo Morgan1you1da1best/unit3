05f99a0b01e984d7e83b85ab8201fb492dad1086
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-10-04 14:17:19 -0400

    Updated from Brython Server: 10/4/2017 2:17:19 PM

--- warmUp9.py
+++ warmUp9.py
@@ -1,1 +1,13 @@
+#Morgan Baughman
+#10/4/17
+#warmUp9.py - Capitalizes all vowels in a word.
+
+word = input('Enter any word: ')
+
+for ch in word:
+    if ch ='= 'a' or 'e' or 'i' or 'u' or 'o':
+        print(ch.upper())
+    else:
+        print(ch.
+
 
