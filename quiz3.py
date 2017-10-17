523f568ce675bce5621a59a31c537d0dcf3c9271
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-10-17 14:38:39 -0400

    Update quiz3.py

--- quiz3.py
+++ quiz3.py
@@ -1,1 +1,21 @@
+#Morgan Baughman
+#10/17/17
+#quiz3.py 
 
+for i in range(-5,6):
+    print(i)
+
+i = 16
+while i<=31:
+    i = i + 2
+    print(i)
+    
+total = 0
+for i in range(13,332,2):
+    total = total + i
+print(total)
+
+while True:
+    word = input('Enter a word: ')
+    if 'z' in word:
+        break
